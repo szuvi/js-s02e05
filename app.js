cd608d5280b361ce1432b74375a2309a3b3051e5
Konrad Trzciński <trzcinskiphoto@gmail.com>
2021-05-17 06:24:49 +0200

    Project starter

---
+++ app.js
@@ -0,0 +1,8 @@
+// You have been giving a task of building and
+//  testing a network. You are receiving commands
+//  and should execute them in order they have
+//  been send. The commands are made from three
+//  elements. Operation B – build connection, T –
+//  test if connection exists and give information
+//  as true or false. The two last elements are
+//  two IP addresses. Start IP and end IP.
